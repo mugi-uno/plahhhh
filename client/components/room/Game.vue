6f218c546d08d05b77eac5648ecf0c123d16bc99
mugi-uno <mugi.uno@gmail.com>
2017-06-03 00:29:30 +0900

    add consensus

--- client/components/room/Game.vue
+++ client/components/room/Game.vue
@@ -24,6 +24,16 @@
     v-bind:players='players'
     v-bind:isOpen='isOpen'
   )
+
+  .consensus(
+    v-if='isOpen && isConsensus'
+  )
+    .text
+      | CONSENSUS
+    .point
+      | {{room.average}}
+      span.point-suffix
+        | pt
 </template>
 
 <script>
@@ -49,7 +59,10 @@ export default {
     },
     isOpen () {
       return this.$props.room.status === "open";
-    } 
+    },
+    isConsensus() {
+      return this.$props.room.result === 'consensus';
+    }
   },
   methods: {
     pointSelect (point) {
@@ -77,4 +90,44 @@ export default {
 .playButton button {
   width: 200px;
 }
-</style>
+
+.consensus {
+  position: absolute;
+  top: 240px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.consensus .text {
+  background-color: hsla(20, 80%, 65%, 0.75);
+  color: white;
+  display: flex;
+  font-size: 6rem;
+  font-weight: bold;
+  height: 110px;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.consensus .point {
+  justify-content: center;
+  width: 100%;
+  display: flex;
+  font-size: 20rem;
+  font-weight: bold;
+  align-items: center;
+  height: 280px;
+  background-color: hsla(20, 80%, 95%, 0.75);
+}
+
+.consensus .point-suffix {
+  margin-left: 10px;
+  font-size: 6rem;
+  margin-top: 117px;
+  margin-right: -100px;
+}
+ </style>
